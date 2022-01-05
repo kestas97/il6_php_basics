710cf394b877779cac39bbf719126d43e08f0e0f
kestas97 <kestutissukys97@gmail.com>
2022-01-05 17:32:26 +0200

    adding fuctions php file

--- functions.php
+++ functions.php
@@ -1,15 +1,82 @@
 <?php
 
-$name = 'Arnoldas';
-$surname = 'Turulis';
 
-// arntur;
+$products = [
+    [
+        'name' => 'Siulai',
+        'price' => 12.4,
+        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+    ],
+
+    [
+        'name' => 'virbalai',
+        'price' => 2.50,
+        'img' => 'https://www.siltassiulas.lt/images/uploader/kn/knitpro-nova-cubics-ilgi-virbalai-35-cm-600-mm-1.jpg'
+    ],
+
+    [
+    'name' => 'adatos',
+    'price' => 1.90,
+        'special_price' => 0.50,
+    'img' => 'http://www.siuvimoreikmenys.lt/upl/Image/Ivairus/apie-adatas.jpg'
+    ],
+
+    [
+    'name' => 'Siulai',
+    'price' => 12.4,
+    'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+    ],
+
+    [
+        'name' => 'virbalai',
+        'price' => 2.50,
+        'img' => 'https://www.siltassiulas.lt/images/uploader/kn/knitpro-nova-cubics-ilgi-virbalai-35-cm-600-mm-1.jpg'
+    ],
+
+    [
+        'name' => 'adatos',
+        'price' => 1.90,
+        'img' => 'http://www.siuvimoreikmenys.lt/upl/Image/Ivairus/apie-adatas.jpg'
+    ],
+
+    [
+        'name' => 'Siulai',
+        'price' => 12.4,
+        'special_price' => 5.99,
+        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+    ],
+
+    [
+        'name' => 'virbalai',
+        'price' => 2.50,
+        'img' => 'https://www.siltassiulas.lt/images/uploader/kn/knitpro-nova-cubics-ilgi-virbalai-35-cm-600-mm-1.jpg'
+    ],
+
+    [
+        'name' => 'adatos',
+        'price' => 1.90,
+        'img' => 'http://www.siuvimoreikmenys.lt/upl/Image/Ivairus/apie-adatas.jpg'
+    ],
+
+    ];
 
 
-function getNickName($name, $surname){
-    $firstNameLetters = substr($name, 0, 3);
-    $firstsurnNameLetters = substr($surname, 0, 3);
-    return $firstNameLetters.$firstsurnNameLetters;
+$counter = 0;
+
+foreach($products as $product) {
+        echo '<img width="60" src="' . $product['img'] . '" />';
+        echo '<h2>' . $product["name"] . '</h2>';
+        if($product['special_price']){
+            echo '<del>' .$product['special_price']. '</del>' .$product['special_price'];
+        }else {
+            echo $product['price'];
+
+        }
+        echo '<h3>' . $product["price"] . '</h3>';
+        //$counter++;
+        //if ($counter % 3 == 0)
+            echo '<hr>';
+
+
 }
-
-echo getNickName($name, $surname);
+echo $counter;
--- index.php
+++ index.php
@@ -1,48 +1,49 @@
 <?php
 
-$string = "Hello World";
-$productName = "Rudeniniai Batai Rudi";
-$productDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam vitae velit id congue. Vivamus sed lacus pulvinar, iaculis purus et, aliquet mauris. Vivamus metus ipsum, tincidunt ut enim et, porttitor tempus quam. Mauris quam arcu, bibendum vel dictum ut, auctor non enim.";
-$productBrand = "Nike";
-$symbol = "@";
-$number = '1';
-
-
-$intiger = 1;
-$intiger2 = 123;
-$intiger3 = 233232;
-
-$productqty = 4;
-$holesCount = 32;
-$productManufacturerYears = 2021;
-
-$productprice = 67.99;
-$size = 41.5;
-$weight = 0.234;
-
-$isInStock = true;
-$waterProof = true;
-$airless = false;
-echo  "<pre>";
-$product = [
-    "name" => $productName,
-    "qty" => $productqty,
-    "price" => $productprice,
-    "water_prof" => $waterProof
-];
-
-$product2 = [
-    "name" => "Vasariniai Batai Balti",
-    "qty" => 1,
-    "price" => 87.99,
-    "water_prof" => true
-];
-
-
-
-
-
-$products = [$product, $product2];
+include "functions.php";
+//$string = "Hello World";
+//$productName = "Rudeniniai Batai Rudi";
+//$productDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam vitae velit id congue. Vivamus sed lacus pulvinar, iaculis purus et, aliquet mauris. Vivamus metus ipsum, tincidunt ut enim et, porttitor tempus quam. Mauris quam arcu, bibendum vel dictum ut, auctor non enim.";
+//$productBrand = "Nike";
+//$symbol = "@";
+//$number = '1';
+//
+//
+//$intiger = 1;
+//$intiger2 = 123;
+//$intiger3 = 233232;
+//
+//$productqty = 4;
+//$holesCount = 32;
+//$productManufacturerYears = 2021;
+//
+//$productprice = 67.99;
+//$size = 41.5;
+//$weight = 0.234;
+//
+//$isInStock = true;
+//$waterProof = true;
+//$airless = false;
+//echo  "<pre>";
+//$product = [
+//    "name" => $productName,
+//    "qty" => $productqty,
+//    "price" => $productprice,
+//    "water_prof" => $waterProof
+//];
+//
+//$product2 = [
+//    "name" => "Vasariniai Batai Balti",
+//    "qty" => 1,
+//    "price" => 87.99,
+//    "water_prof" => true
+//];
+//
+//
+//
+//
+//
+//$products = [$product, $product2];
 
 //echo $product["name"];
 
@@ -68,10 +69,16 @@ $products = [$product, $product2];
  */
 
 // +-/ * %;
-$pvm = 21;
-$priceWithTax = $productprice * (1+($pvm / 100));
+//$pvm = 21;
+//$priceWithTax = $productprice * (1+($pvm / 100));
 
-echo $priceWithTax;
+//echo $priceWithTax;
+
+
+
+
+
+
 
 
 
