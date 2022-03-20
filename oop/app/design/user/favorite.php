7b595f1b1922fd2c4d4c4941a127244d05531413
kestas97 <kestutissukys97@gmail.com>
2022-03-20 15:51:11 +0200

    starting create csv import

--- oop/app/design/user/favorite.php
+++ oop/app/design/user/favorite.php
@@ -1,12 +1,12 @@
 <div class="wrapper-favorite">
     <ul>
         <?php foreach ($this->data['list'] as $ad): ?>
-        <a>
-            <li>
-            <a href="<?= $this->url('catalog/show/' .$ad->getSlug()) ?>"
-               <?= $ad->getTitle() ?>
-        </a>
-        </li>
+
+                <li>
+                    <a href="<?= $this->url('catalog/show/' . $ad->getSlug()) ?>">
+                <?= $ad->getTitle() ?>
+                    </a>
+            </li>
         <?php endforeach; ?>
     </ul>
 </div>
