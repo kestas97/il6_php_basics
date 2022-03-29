d99a6a5b8b5c6b778e30a0c4b6603e17c7db1523
kestas97 <kestutissukys97@gmail.com>
2022-03-29 16:32:32 +0300

    new project

---
+++ gandas.lt/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '04266fc2e461020106bb0221505852114ead7d9a',
+        'name' => 'kestas/gandas.lt',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kestas/gandas.lt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '04266fc2e461020106bb0221505852114ead7d9a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
